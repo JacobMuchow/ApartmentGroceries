da6d0b4b4f59363efdc17b1cb9317e7a071d3e6b
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-10-19 17:56:19 -0500

    added createdBy and groupId when adding grocery item and moved logout to home activity

--- app/src/main/java/com/quarkworks/apartmentgroceries/main/HomeActivity.java
+++ app/src/main/java/com/quarkworks/apartmentgroceries/main/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.quarkworks.apartmentgroceries.main;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,11 +11,13 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.quarkworks.apartmentgroceries.R;
+import com.quarkworks.apartmentgroceries.auth.LoginActivity;
 import com.quarkworks.apartmentgroceries.grocery.AddGroceryItemActivity;
 import com.quarkworks.apartmentgroceries.grocery.GroceryCell;
 import com.quarkworks.apartmentgroceries.group.GroupActivity;
 import com.quarkworks.apartmentgroceries.service.DataStore;
 import com.quarkworks.apartmentgroceries.service.SyncGroceryItem;
+import com.quarkworks.apartmentgroceries.service.SyncUser;
 import com.quarkworks.apartmentgroceries.service.models.RGroceryItem;
 
 import io.realm.RealmBaseAdapter;
@@ -65,6 +68,14 @@ public class HomeActivity extends AppCompatActivity {
                 return true;
             case R.id.action_list_group:
                 intent = new Intent(this, GroupActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_logout:
+                SyncUser.logout();
+                SharedPreferences sharedPreferences =
+                        this.getSharedPreferences(getApplication().getString(R.string.login_or_sign_up_session), 0);
+                sharedPreferences.edit().clear().commit();
+                intent = new Intent(this, LoginActivity.class);
                 startActivity(intent);
                 return true;
         }
