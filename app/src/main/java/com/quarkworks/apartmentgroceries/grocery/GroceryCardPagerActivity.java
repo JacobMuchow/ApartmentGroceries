42ce4420e71ff03bc70fcb2538c8b70f66e280b4
Zhaolong Zhong <zztg2@mail.missouri.edu>
2015-10-24 16:21:50 -0500

    added page transformer

--- app/src/main/java/com/quarkworks/apartmentgroceries/grocery/GroceryCardPagerActivity.java
+++ app/src/main/java/com/quarkworks/apartmentgroceries/grocery/GroceryCardPagerActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.quarkworks.apartmentgroceries.R;
 import com.quarkworks.apartmentgroceries.service.DataStore;
@@ -40,6 +41,7 @@ public class GroceryCardPagerActivity extends AppCompatActivity {
          * Get view references
          */
         viewPager = (ViewPager) findViewById(R.id.grocery_card_pager_view_pager_id);
+        viewPager.setPageTransformer(true, new zoomOutPageTransformer());
 
         groceryItems = DataStore.getInstance().getRealm().where(RGroceryItem.class).findAll();
         NUM_PAGES = groceryItems.size();
@@ -65,6 +67,42 @@ public class GroceryCardPagerActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return NUM_PAGES;
+        }
+    }
+
+    public class zoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+
+            if (position < -1) {
+                // left most
+                view.setAlpha(0);
+
+            } else if (position <= 1) {
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float verticalMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horizontalMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    view.setTranslationX(horizontalMargin - verticalMargin / 2);
+                } else {
+                    view.setTranslationX(-horizontalMargin + verticalMargin / 2);
+                }
+
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                view.setAlpha(MIN_ALPHA +
+                        (scaleFactor - MIN_SCALE) /
+                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+            } else {
+                // right most
+                view.setAlpha(0);
+            }
         }
     }
 }
